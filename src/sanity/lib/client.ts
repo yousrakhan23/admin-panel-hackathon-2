c01b9b16f43e3d35f993942cdf20c3aeb32d1e6c
yousrakhan23 <ammarahkhan2016@gmail.com>
2025-02-08 22:31:16 +0500

    update some files

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,7 +1,5 @@
 import { createClient } from 'next-sanity'
 
-import { apiVersion, dataset, projectId } from '../env'
-
 export const client = createClient({
   projectId:"b8cf56un",
   dataset:"production",
